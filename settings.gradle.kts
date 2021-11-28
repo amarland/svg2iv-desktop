90a882ce4e7d95913783c1f6c044480f16f1e039
Anthony Marland <anthony.marland@outlook.com>
2021-11-28 17:09:21 +0100

    Updated Gradle build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,12 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
